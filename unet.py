097496ebfba0932567a81751b6b8485341bb9f8c
hylee <hylee719@stanford.edu>
2023-06-07 13:03:28 -0700

    changes

--- unet.py
+++ unet.py
@@ -320,59 +320,87 @@ import torch.nn as nn
 
 
 class UNet(nn.Module):
-    def __init__(self, in_channels=3, out_channels=1, init_features=32):
+    def __init__(self, in_channels=3, out_channels=1, init_features=256):
         super(UNet, self).__init__()
 
         features = init_features
-        self.upconv1 = nn.ConvTranspose2d(
-            in_channels, features * 8, kernel_size=2, stride=2
-        )
-        self.decoder1 = UNet._block(features * 16, features * 8, name="dec1")
-        self.upconv2 = nn.ConvTranspose2d(
-            features * 8, features * 4, kernel_size=2, stride=2
-        )
-        self.decoder2 = UNet._block(
-            features * 8, features * 4, name="dec2")
-        self.upconv3 = nn.ConvTranspose2d(
-            features * 4, features * 2, kernel_size=2, stride=2
-        )
-        self.decoder3 = UNet._block(
-            features * 4, features * 2, name="dec3")
-        self.upconv4 = nn.ConvTranspose2d(
-            features * 2, features, kernel_size=2, stride=2
-        )
-        self.decoder4 = UNet._block(
-            features * 2, features, name="dec4")
+
+        self.encoder1 = UNet._block(in_channels, features, name="enc1")
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.encoder2 = UNet._block(features, features // 2, name="enc2")
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.encoder3 = UNet._block(features // 2, features // 4, name="enc3")
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.encoder4 = UNet._block(features // 4, features // 8, name="enc4")
+        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.bottleneck = UNet._block(
-            features, features, name="bottleneck")
+            features // 8, features // 4, name="bottleneck")
+
+        self.upconv4 = nn.ConvTranspose2d(
+            features // 4, features // 8, kernel_size=2, stride=2
+        )
+        self.decoder4 = UNet._block(
+            features // 8 * 2, features // 8, name="dec4")
+        self.upconv3 = nn.ConvTranspose2d(
+            features // 8, features // 4, kernel_size=2, stride=2
+        )
+        self.decoder3 = UNet._block(
+            features // 4 * 2, features // 4, name="dec3")
+        self.upconv2 = nn.ConvTranspose2d(
+            features // 4, features // 2, kernel_size=2, stride=2
+        )
+        self.decoder2 = UNet._block(
+            features // 2 * 2, features // 2, name="dec2")
+        self.upconv1 = nn.ConvTranspose2d(
+            features // 2, features, kernel_size=2, stride=2
+        )
+        self.decoder1 = UNet._block(features * 2, features, name="dec1")
 
         self.conv = nn.Conv2d(
             in_channels=features, out_channels=out_channels, kernel_size=1
         )
 
     def forward(self, x):
-        print("shape before upconv1: ", x.shape)
-        dec1 = self.upconv1(x)
-        print("shape before decoder1: ", dec1.shape)
-        dec1 = self.decoder1(dec1)
-        print("shape before upconv2: ", dec1.shape)
-        dec2 = self.upconv2(dec1)
-        print("shape before decoder2: ", dec2.shape)
-        dec2 = self.decoder2(dec2)
-        print("shape before upconv3: ", dec2.shape)
-        dec3 = self.upconv3(dec2)
-        print("shape before decoder3: ", dec3.shape)
-        dec3 = self.decoder3(dec3)
-        print("shape before upconv4: ", dec3.shape)
-        dec4 = self.upconv4(dec3)
-        print("shape before decoder4: ", dec4.shape)
+        print("shape before enc1: ", x.shape)
+        enc1 = self.encoder1(x)
+        print("shape before pool1: ", enc1.shape)
+        pool1 = self.pool1(enc1)
+        print("shape before enc2: ", pool1.shape)
+        enc2 = self.encoder2(pool1)
+        print("shape before pool2: ", enc2.shape)
+        pool2 = self.pool2(enc2)
+        print("shape before enc3: ", pool2.shape)
+        enc3 = self.encoder3(pool2)
+        print("shape before pool3: ", enc3.shape)
+        pool3 = self.pool3(enc3)
+        print("shape before enc4: ", pool3.shape)
+        enc4 = self.encoder4(pool3)
+        print("shape before pool4: ", enc4.shape)
+        pool4 = self.pool4(enc4)
+        print("shape before bottleneck: ", pool4.shape)
+
+        bottleneck = self.bottleneck(pool4)
+        print("shape before upconv4: ", bottleneck.shape)
+
+        dec4 = self.upconv4(bottleneck)
+        dec4 = torch.cat((dec4, enc4), dim=1)
         dec4 = self.decoder4(dec4)
-        print("shape before bottleneck: ", dec4.shape)
+        print("shape before upconv3: ", dec4.shape)
+        dec3 = self.upconv3(dec4)
+        dec3 = torch.cat((dec3, enc3), dim=1)
+        dec3 = self.decoder3(dec3)
+        print("shape before upconv2: ", dec3.shape)
+        dec2 = self.upconv2(dec3)
+        dec2 = torch.cat((dec2, enc2), dim=1)
+        dec2 = self.decoder2(dec2)
+        print("shape before dec2: ", dec2.shape)
+        dec1 = self.upconv1(dec2)
+        dec1 = torch.cat((dec1, enc1), dim=1)
+        dec1 = self.decoder1(dec1)
+        print("shape before final conv and sigmoid: ", dec1.shape)
 
-        bottleneck = self.bottleneck(dec4)
-
-        return torch.sigmoid(self.conv(bottleneck))
+        return torch.sigmoid(self.conv(dec1))
 
     @staticmethod
     def _block(in_channels, features, name):
